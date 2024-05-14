7a20e720f54f75c2380ec06e50f8cefee04414f4
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-05-14 11:22:05 +0300

    global registration

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// import CoffeePlan from './components/CoffeePlan.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+// .component("CoffeePlan", CoffeePlan) // Global registration
+.mount('#app')
